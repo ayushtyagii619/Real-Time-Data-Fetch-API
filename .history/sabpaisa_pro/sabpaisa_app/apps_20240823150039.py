7919814847bc32a6eefd01116caa9aca437de6d1
Ayush <silentfeature1999@gmail.com>
2024-08-28 11:47:20 +0530

    new update

--- .history/sabpaisa_pro/sabpaisa_app/apps_20240823150039.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class SabpaisaAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sabpaisa_app'
-    AUTH_USER_MODEL = 'sabpaisa_app.AyushUser'
