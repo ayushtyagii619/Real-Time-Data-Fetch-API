e59d74f81a0e50a363fa5bda150be8b5433ea1cb
Govind Tyagi <arpittyagi886898@gmail.com>
2024-08-30 09:27:52 +0000

    settings.py edited online with Bitbucket

--- sabpaisa_pro/sabpaisa_pro/settings.py
+++ sabpaisa_pro/sabpaisa_pro/settings.py
@@ -76,7 +76,7 @@ ROOT_URLCONF = 'sabpaisa_pro.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -159,9 +159,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
