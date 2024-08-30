b3543e5bf4d2c93d8a6d3b20fcca6e82535285bc
Govind Tyagi <arpittyagi886898@gmail.com>
2024-08-30 09:18:43 +0000

    settings.py edited online with Bitbucket

--- sabpaisa_pro/sabpaisa_pro/settings.py
+++ sabpaisa_pro/sabpaisa_pro/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4g627_%o=ckhzj0*(nj^yu3oym6r&)b-9war-+lb!9r=p5!zy7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['*']
