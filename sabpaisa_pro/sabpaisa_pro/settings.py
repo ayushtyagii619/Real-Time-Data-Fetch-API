50500127f3a1e0581e2ebd23839d163ada29d6d9
Govind Tyagi <arpittyagi886898@gmail.com>
2024-08-30 08:09:45 +0000

    settings.py edited online with Bitbucket

--- sabpaisa_pro/sabpaisa_pro/settings.py
+++ sabpaisa_pro/sabpaisa_pro/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-4g627_%o=ckhzj0*(nj^yu3oym6r&)b-9war-+lb!9r=p5!zy7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '10.3.2.142',
+    # Add other hosts if needed, e.g., 'yourdomain.com'
+]
+
 
 
 # Application definition
